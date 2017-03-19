81b792890bd074c78c8a7224291f4baa7f79523a
Davide Dal Farra <dalfarra@codref.com>
2017-03-19 19:36:19 +0100

    added disableVisibilityChange to stage

--- src/states/boot.js
+++ src/states/boot.js
@@ -5,6 +5,7 @@ export default class extends Phaser.State {
 
   create () {
     // game.scale.setGameSize(900, 400);
+    this.stage.disableVisibilityChange = true;
     this.state.start('Load');
   }
 
